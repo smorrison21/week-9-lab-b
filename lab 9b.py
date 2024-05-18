a956bfe83246d9a2231d255899cf9e08b4bd58c2
smorrison21 <164384120+smorrison21@users.noreply.github.com>
2024-05-17 22:00:12 -0500

    Update lab 9b.py

--- lab 9b.py
+++ lab 9b.py
@@ -1,1 +1,18 @@
 
+# Sarah Morrison
+
+# Create your own github repo
+# Add a .py file
+# Create a simple version of an agent based simulation, based on the code in the last lecture, and the github repo linked at the end
+# 1. Create an Agent class
+# 2. Create a World class
+# 3. Initialize the world
+# 4. Create a loop
+# Ask each agent in sequence to find an empty patch
+# Move the agent to the empty patch
+# 5. End
+# Keep it simple (small grid, small number of agents, small number of loops), and utilize the code from the more complex example given in lecture. 
+
+ 
+
+
